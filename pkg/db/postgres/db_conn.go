5e00683d7170fbb1d7f7654b1777c4eee022ab48
Reza Amiri <reza.amiri.ac@gmail.com>
2022-09-19 13:53:52 +0300

    added db to pkg

---
+++ pkg/db/postgres/db_conn.go
@@ -0,0 +1,51 @@
+package postgres
+
+import (
+	"fmt"
+	"time"
+
+	_ "github.com/jackc/pgx/stdlib" // pgx driver
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	maxOpenConns    = 60
+	connMaxLifetime = 120
+	maxIdleConns    = 30
+	connMaxIdleTime = 20
+)
+
+type Config struct {
+	Driver   string `mapstructure:"POSTGRES_DRIVER"`
+	Host     string `mapstructure:"POSTGRES_HOST"`
+	Port     string `mapstructure:"POSTGRES_PORT"`
+	User     string `mapstructure:"POSTGRES_USER"`
+	DBName   string `mapstructure:"POSTGRES_DB_NAME"`
+	Password string `mapstructure:"POSTGRES_PASSWORD"`
+}
+
+// Return new Postgresql db instance
+func NewPsqlDB(c Config) (*sqlx.DB, error) {
+	dataSourceName := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		c.Host,
+		c.Port,
+		c.User,
+		c.DBName,
+		c.Password,
+	)
+
+	db, err := sqlx.Connect(c.Driver, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetConnMaxLifetime(connMaxLifetime * time.Second)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxIdleTime(connMaxIdleTime * time.Second)
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
--- pkg/go.mod
+++ pkg/go.mod
@@ -3,6 +3,8 @@ module github.com/rezaAmiri123/microservice/pkg
 go 1.18
 
 require (
+	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/segmentio/kafka-go v0.4.35
 	github.com/uber/jaeger-client-go v2.30.0+incompatible
@@ -12,10 +14,16 @@ require (
 
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gofrs/uuid v4.3.0+incompatible // indirect
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
 	github.com/klauspost/compress v1.15.7 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
