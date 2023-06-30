f00e6eefaa5c63dca8ce90671b82f4a08db4027b
Reza Amiri <reza.amiri.ac@gmail.com>
2023-06-30 11:39:31 +0300

    vendor

--- pkg/db/postgres/migrate_v4.go
+++ pkg/db/postgres/migrate_v4.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
+	_ "github.com/lib/pq"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
