b6c516f07cd0b152768407c27cc8449b20a76677
Reza Amiri <reza.amiri.ac@gmail.com>
2023-08-03 17:27:37 +0300

    build deploytools

--- deploy/setup-tools/set-tool-alias.sh
+++ deploy/setup-tools/set-tool-alias.sh
@@ -2,10 +2,20 @@
 
 root=$(realpath ${PWD}/../..)
 
-docker build -t deploytools:latest .
+# docker build -t deploytools:latest .
 
-alias deploytools='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ~/.aws:/root/.aws -v ~/.docker:/root/.docker -v ~/.kube:/root/.kube -v ${root}:/mallbots -v ${PWD}:/mallbots/deployment/.current -w /mallbots/deployment/.current deploytools'
+# alias deploytools='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ~/.aws:/root/.aws -v ~/.docker:/root/.docker -v ~/.kube:/root/.kube -v ${root}:/mallbots -v ${PWD}:/mallbots/deployment/.current -w /mallbots/deployment/.current deploytools'
 
-echo "---"
-echo
-echo "Usage: deploytools <cmd [parameters]>"
+# echo "---"
+# echo
+# echo "Usage: deploytools <cmd [parameters]>"
+
+docker run --rm -it \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v ~/.aws:/root/.aws \
+        -v ~/.docker:/root/.docker \
+        -v ~/.kube:/root/.kube \
+        -v ${root}:/mallbots \
+        -v ${PWD}:/mallbots/deployment/.current \
+        -w /mallbots/deployment/.current \
+        deploytools
